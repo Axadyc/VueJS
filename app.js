a72f2f688d28c49ffd8696328b47e987ce565a8a
Axadyc <adamlmr62@gmail.com>
2023-12-18 17:20:00 +0100

    v-model v-for

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var app = new Vue({
   el: "#app",
 
   data:{
-    user : "Adamos"
+    user : "Adamos",
+    pokemonList : [{name : "pikachu"},{name : "salamèche"}, {name : "reptincel"},{name : "dracaufeu"}]
   }
 
 });
